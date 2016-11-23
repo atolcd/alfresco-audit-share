50fdd3b67bcb0f0ae9e151a9213a724a804e511d
Laurent Meunier <lme@atolcd.com>
2016-11-23 12:47:09 +0100

    Pagination de la recherche dans le calcul de la volumétrie
    La quantité de mémoire consommée par la recherche CMIS est directement
    liée au nombre de documents présents les sites. Si un site contient trop
    de documents, les caches sont saturés et la heap de la JVM est remplie.
    Utilise searchParameters.setSkipcount(int) pour découper la recherche en
    lot de 1000 résultats (par défaut - modifiable via
    share.stats.sites.volumetry.batchSize).
    
    Change-Id: I95c52474dac35b096a3bc6ecd9d887c790e8d652
    (cherry picked from commit 7b2a4798c60079f30057b312c8571c9256e8e50f)

--- module_alfresco/src/main/java/com/atolcd/alfresco/repo/jscript/ShareStats.java
+++ module_alfresco/src/main/java/com/atolcd/alfresco/repo/jscript/ShareStats.java
@@ -43,6 +43,7 @@ public class ShareStats extends BaseScopableProcessorExtension implements Initia
   private InsertAuditPost  wsInsertAudits;
   protected SiteService    siteService;
   protected SearchService  searchService;
+  protected int            batchSize;
 
   public SiteService getSiteService() {
     return siteService;
@@ -58,6 +59,14 @@ public class ShareStats extends BaseScopableProcessorExtension implements Initia
 
   public void setSearchService(SearchService searchService) {
     this.searchService = searchService;
+  }
+
+  public int getBatchSize() {
+    return batchSize;
+  }
+
+  public void setBatchSize(int batchSize) {
+    this.batchSize = batchSize;
   }
 
   public void setWsInsertAudits(InsertAuditPost wsInsertAudits) {
@@ -105,7 +114,7 @@ public class ShareStats extends BaseScopableProcessorExtension implements Initia
     searchParameters.setLimitBy(LimitBy.UNLIMITED);
     searchParameters.setMaxPermissionChecks(Integer.MAX_VALUE);
     searchParameters.setMaxPermissionCheckTimeMillis(Long.MAX_VALUE);
-    searchParameters.setMaxItems(-1);
+    searchParameters.setMaxItems(batchSize);
     long totalSize = 0;
     List<AtolVolumetryEntry> listAtolVolEntry = new ArrayList<>();
     for (SiteInfo siteInfo : siteInfos) {
@@ -120,25 +129,41 @@ public class ShareStats extends BaseScopableProcessorExtension implements Initia
     String query = "SELECT * FROM cmis:document D WHERE CONTAINS(D,'PATH: \"/app:company_home/st:sites/cm:" + siteInfo.getShortName()
         + "//*\"')";
     searchParameters.setQuery(query);
-    ResultSet rs = null;
+
     long totalSize = 0;
     int nbFile = 0;
-    try {
-      rs = searchService.query(searchParameters);
+    int skipCount = 0;
 
-      for (ResultSetRow rsr : rs) {
-        // Useful to avoid nullpointer at the following line
-        @SuppressWarnings("unused")
-        Map<String, Serializable> mapValue = rsr.getValues();
-        ContentDataWithId docContent = (ContentDataWithId) rsr.getValue(ContentModel.PROP_CONTENT);
-        if (docContent != null) {
-          totalSize = totalSize + docContent.getSize();
-          nbFile++;
+    while (true) {
+      searchParameters.setSkipCount(skipCount);
+      ResultSet rs = null;
+      try {
+        rs = searchService.query(searchParameters);
+
+        if (rs.length() == 0) {
+          // we are at the end of our search, no more results are available
+          break;
         }
-      }
-    } finally {
-      if (rs != null) {
-        rs.close();
+
+        for (ResultSetRow rsr : rs) {
+          // Useful to avoid nullpointer at the following line
+          @SuppressWarnings("unused")
+          Map<String, Serializable> mapValue = rsr.getValues();
+          ContentDataWithId docContent = (ContentDataWithId) rsr.getValue(ContentModel.PROP_CONTENT);
+          if (docContent != null) {
+            totalSize = totalSize + docContent.getSize();
+            nbFile++;
+          }
+        }
+        skipCount += rs.length();
+
+        if (logger.isDebugEnabled()) {
+          logger.debug("  -- Site Volumetry " + siteInfo.getShortName() + ": " + skipCount);
+        }
+      } finally {
+        if (rs != null) {
+          rs.close();
+        }
       }
     }
     return new AtolVolumetryEntry(siteInfo.getShortName(), totalSize, 0, nbFile, atTime);
