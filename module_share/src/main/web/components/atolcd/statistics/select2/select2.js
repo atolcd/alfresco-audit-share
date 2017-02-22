655efd130eafd7b4565d4db2ac41e97cee2b4ccb
apr <apr@atolcd.com>
2017-02-22 11:54:52 +0100

    Modification de 'select2.js' pour éviter les erreurs 'dojo'
    - Mises en commentaire des instructions des lignes 9 à 12 afin d'éviter
      des erreurs 'dojo' au moment du chargement de select2.
    
    Issue #29846
    
    Change-Id: I592646717e4238d97ce3943d351a09e12119c41d

--- module_share/src/main/web/components/atolcd/statistics/select2/select2.js
+++ module_share/src/main/web/components/atolcd/statistics/select2/select2.js
@@ -6,10 +6,10 @@
  * https://github.com/select2/select2/blob/master/LICENSE.md
  */
 (function (factory) {
-  if (typeof define === 'function' && define.amd) {
+  /*if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
     define(['jquery'], factory);
-  } else if (typeof exports === 'object') {
+  } else*/ if (typeof exports === 'object') {
     // Node/CommonJS
     factory(require('jquery'));
   } else {
