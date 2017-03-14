2b47e0ada2406f8a0490e922ae1e75f4634a8ee1
apr <apr@atolcd.com>
2017-03-14 13:57:05 +0100

    Prise en compte des groupes dans l'export CSV
    - Ajout du nom du(des) groupe(s) correspondant(s) aux
      données exportées.
    
    Issue #31970
    
    Change-Id: Idd01bdb9b731e3f365f2424dbe43f3f7915250b0

--- module_alfresco/src/main/java/com/atolcd/auditshare/repo/service/AuditShareReferentielService.java
+++ module_alfresco/src/main/java/com/atolcd/auditshare/repo/service/AuditShareReferentielService.java
@@ -7,6 +7,8 @@ import com.atolcd.auditshare.repo.xml.Group;
 
 public interface AuditShareReferentielService {
 
+  public static String auditShareReferentielNodeUUID = "auditshare-user-connections-groups";
+
   public List<Group> parseReferentielGroups(InputStream file);
 
   public List<Group> parseRefentielForNodeUUID(String id);
